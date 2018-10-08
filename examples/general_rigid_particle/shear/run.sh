386d2886d6de3058f3de1392c95c9953dad838c8
jyli <jyli@uchicago.edu>
2018-10-08 13:48:48 -0500

    chores: reduce num of timesteps to 100 for an example system

--- examples/general_rigid_particle/shear/run.sh
+++ examples/general_rigid_particle/shear/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-../../../src/copss-RIGIDPARTICLE-opt -pc_type lu -pc_factor_mat_solver_package superlu_dist --perflog-off
+mpirun -n 4 ../../../src/copss-RIGIDPARTICLE-opt                  \
+--disable-perflog                          \
+-ksp_type preonly                           \
+-pc_type lu                                 \
+-pc_factor_mat_solver_package superlu_dist  \
+-ksp_monitor -eps_monitor \
+-memory_view -log_view 2>&1 | tee log_history.txt
 
+#--disable-perflog                          \
